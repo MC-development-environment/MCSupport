0ab1592c0b0d95f2eb64f1b7d50e4c9452a943d8
Heri Esmer Espinosa Reyes <heri.espinosa@multicomputos.com>
2025-12-11 12:11:14 -0400

    Update

--- git-up.bat
+++ git-up.bat
@@ -1,11 +1,11 @@
 
-// Configurar SSH para GitHub
+REM Configurar SSH para GitHub
 git config core.sshCommand "ssh -i ~/.ssh/github-for-job"
 git remote set-url origin git@github-for-job:MC-Development-Environment/MCSupport.git
 
-// Agregar y commitear cambios
+REM Agregar y commitear cambios
 git add .
 git commit -m "Update"
 
-// Subir cambios a GitHub
-git push origin main
+REM Subir cambios a GitHub
+git push origin master
